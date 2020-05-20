0bc84847e8b2d2e8d467dd1a5acc079c917a4a11
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-20 12:10:02 -0400

    finish isPostExisting query

--- backend/queries/posts/posts.js
+++ backend/queries/posts/posts.js
@@ -1,8 +1,29 @@
+const db = require("../../db/db")
+
 module.exports = {
     isPostExisting: async (req, res, next) => {
-
+        const getId = req.params.id;
+        const postId = req.body.post_id;
+        const id = getId ? getId : postId;
+        try {
+            if(id) {
+                next();
+            } else {
+                let post = await db.one("Select * FROM posts where id=$1", id);
+                next();
+            }
+        } catch (error) {
+            if(error.received === 0) {
+                res.status(404).json({
+                    status: 404,
+                    error: `Post ID: ${id} doesn't exist`
+                })
+            } else {
+                next(error);
+            }
+        }
     },
-    
+
     getAllPosts: async (req, res, next) => {
         
     },
--- backend/routes/posts/posts.js
+++ backend/routes/posts/posts.js
@@ -1,6 +1,7 @@
 const posts = require("express").Router();
 
 const { 
+    isPostExisting,
     getAllPosts, 
     getPostById, 
     createPost, 
@@ -9,10 +10,10 @@ const {
 } = require( '../../queries/posts/posts');
 
 posts.get("/", getAllPosts);
-posts.get("/:id", getPostById);
+posts.get("/:id", isPostExisting, getPostById);
 posts.post("/", createPost);
-posts.delete("/:id", deletePost);
+posts.delete("/:id", isPostExisting, deletePost);
 
-posts.get("/:id/tags", getPostTags);
+posts.get("/:id/tags", isPostExisting, getPostTags);
 
 module.exports = posts;
