04e15f3247aa04873a18dc639d6769082f7b1983
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-21 05:58:54 -0400

    create basic Login page components

---
+++ frontend/src/components/Login/LogInForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LogInForm = () => {
+    return (
+        <h1>LogInForm</h1>
+    )
+}
+
+export default LogInForm;
---
+++ frontend/src/components/Login/SignUpForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUpForm = () => {
+    return (
+        <h1>SignUpForm</h1>
+    )
+}
+
+export default SignUpForm;
