54d1a9434fa6c33be5a746431fa34b7a2659e4a0
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-21 06:28:33 -0400

    create Log In and Sign Up forms

--- frontend/src/components/Login/LogInForm.js
+++ frontend/src/components/Login/LogInForm.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const LogInForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const history = useHistory();
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            // sign in with firebase then change route
+            history.push("/")
+        } catch (error) {
+            setError(error.message);
+        }
+            
+    }
+        
     return (
-        <h1>LogInForm</h1>
+        <>
+            {error ? <div>{error}</div> : null}
+            <form className="logInForm" onSubmit={handleSubmit}>
+                <input type="email" placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                />
+    
+                <input type="password" placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.currentTarget.value)}
+                    autoComplete="on"
+                />
+    
+                <input type="submit" value="Log In"/>
+            </form>
+        </>
     )
 }
 
--- frontend/src/components/Login/SignUpForm.js
+++ frontend/src/components/Login/SignUpForm.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { apiURL } from '../../util/apiURL';
 
 const SignUpForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const history = useHistory();
+    const API = apiURL();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            // sign up with firebase and send results to our backend
+            history.push("/")
+        } catch (error) {
+            setError(error.message);
+        }
+        
+    }
+    
     return (
-        <h1>SignUpForm</h1>
+        <>
+            {error ? <div>{error}</div> : null}
+            <form className="signUpForm" onSubmit={handleSubmit}>
+                <input type="email" placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                />
+
+                <input type="password" placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.currentTarget.value)}
+                    autoComplete="on"
+                />
+
+                <input type="submit" value="Sign Up"/>
+            </form>
+        </>
     )
 }
 
