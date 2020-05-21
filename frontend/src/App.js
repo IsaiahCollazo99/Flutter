a426ed192531f4c864edac9644a5af1c5e5a8c0c
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-21 06:08:25 -0400

    setup basic Profile component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NavBar from './components/General/NavBar';
 import { Route } from 'react-router-dom';
 import LogIn from './components/Login/LogIn';
+import Profile from './components/Profile/Profile';
 
 function App() {
     return (
@@ -15,6 +16,10 @@ function App() {
 
             <Route path={"/login"}>
                 <LogIn />
+            </Route>
+
+            <Route path={"/profile"}>
+                <Profile />
             </Route>
             
         </div>
