b5555b6ea7ac09a84a4f456a7976c902af0a03d5
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-22 16:18:43 -0400

    functionality to make posts and add into database

--- frontend/src/components/FeedPage/FeedPage.js
+++ frontend/src/components/FeedPage/FeedPage.js
@@ -31,8 +31,40 @@ const FeedPage = () => {
         getAllPost();
     })
 
-    const makePostSubmit = (postBody) => {
-        debugger;
+    const findTags = (postBody) => {
+        let tags = []
+        let tagFound = false;
+        let currTag = "";
+        
+        for(let char of postBody) {
+            if(char === "#") {
+                tagFound = true;
+            }
+
+            if(tagFound = true) {
+                if(char === " ") {
+                    tagFound = false;
+                    tags.push(currTag);
+                    currTag = "";
+                } else {
+                    currTag += char
+                }
+            }
+        }
+
+        return tags.length ? tags : null
+    }
+
+    const makePostSubmit = async (postBody) => {
+        let tags = findTags(postBody);
+        let res = await axios.post(API + "/api/posts", {
+            poster_id: currentUser.id, 
+            body: postBody,
+            tags, 
+            created_at: new Date().toString()
+        })
+       
+        getAllPost();
     }
 
     return (
