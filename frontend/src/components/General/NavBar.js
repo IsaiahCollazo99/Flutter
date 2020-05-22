0d7386904de72f6e2464b67b3b50f3622831fd9e
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-22 17:40:20 -0400

    change /profile to go to /username of the user logged in

--- frontend/src/components/General/NavBar.js
+++ frontend/src/components/General/NavBar.js
@@ -1,17 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 import '../../css/general/NavBar.css';
 import { logOut } from '../../util/firebaseFunctions';
 import { AuthContext } from '../../providers/AuthContext';
+import { apiURL } from '../../util/apiURL';
 
 const NavBar = () => {
     const { currentUser } = useContext(AuthContext);
+    const API = apiURL();
+    const [ currentUsername, setCurrentUsername ] = useState("");
+
+    const getUsername = async () => {
+        let res = await axios.get(API + "/api/users/" + currentUser.id);
+        setCurrentUsername(res.data.user.username);
+    }
+
+    useEffect(() => {
+        getUsername();
+    }, [])
 
     const displayButtons = () => {
         if(currentUser) {
             return (
                 <>
-                    <NavLink exact to={"/profile"}>Profile</NavLink>
+                    <NavLink exact to={`/${currentUsername}`}>Profile</NavLink>
                     <button onClick={logOut}>Log Out</button>
                 </>
             )
--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Profile = () => {
+    const { userName } = useParams();
     return (
-        <p>PROFILE</p>
+        <p>{userName}</p>
     )
 }
 
