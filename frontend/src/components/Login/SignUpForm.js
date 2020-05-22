8c74ab80ed54d752c9192f75855cb221640209cd
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-22 11:14:39 -0400

    add extra inputs to the sign up form

--- frontend/src/components/Login/SignUpForm.js
+++ frontend/src/components/Login/SignUpForm.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { apiURL } from '../../util/apiURL';
 import { signUp } from '../../util/firebaseFunctions'
+import { useInput } from '../../util/customHooks';
 
 const SignUpForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const email = useInput("");
+    const password = useInput("");
+    const name = useInput("");
+    const username = useInput("");
+    
     const [error, setError] = useState(null);
     const history = useHistory();
     const API = apiURL();
@@ -14,8 +18,8 @@ const SignUpForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            let res = await signUp(email, password);
-            await axios.post(API + "/api/users", {id: res.user.uid, email})
+            let res = await signUp(email.value, password.value);
+            await axios.post(API + "/api/users", {id: res.user.uid, email: email.value})
             history.push("/")
         } catch (error) {
             setError(error.message);
@@ -27,16 +31,10 @@ const SignUpForm = () => {
         <>
             {error ? <div>{error}</div> : null}
             <form className="signUpForm" onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.currentTarget.value)}
-                />
-
-                <input type="password" placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.currentTarget.value)}
-                    autoComplete="on"
-                />
+                <input type="email" placeholder="Email" {...email} autoComplete="on"/>
+                <input type="password" placeholder="Password" {...password} autoComplete="on" />
+                <input type="text" placeholder="Name" {...name} autoComplete="on"/>
+                <input type="text" placeholder="Username" {...username} autoComplete="on"/>
 
                 <input type="submit" value="Sign Up"/>
             </form>
---
+++ frontend/src/util/customHooks.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export const useInput = ( initialValue ) => {
+    const [value, setValue] = useState(initialValue);
+    
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    return {value, onChange: handleChange}
+}
