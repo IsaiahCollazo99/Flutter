8f5a46628b9d61c9e7b9870f5d04b6f88bc95fab
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-27 06:14:27 -0400

    create UserCard styling

--- frontend/src/components/General/UserCard.js
+++ frontend/src/components/General/UserCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/general/UserCard.css';
 
 const UserCard = ({ user }) => {
     return (
         <div className="userCardContainer">
-            <p>{user.full_name}</p>
-            <p>{user.username}</p>
+            <Link to={"/" + user.username} className="userCardName">{user.full_name}</Link>
+            <p className="userCardUsername">@{user.username}</p>
         </div>
     )
 }
