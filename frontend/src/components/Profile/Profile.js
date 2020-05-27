2494ff5a6ef2cf65ab71b5c37aedbe5d3577660f
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-27 10:26:19 -0400

    add profile picture to Profile component

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -5,6 +5,7 @@ import { apiURL } from '../../util/apiURL';
 import { AuthContext } from '../../providers/AuthContext';
 import Post from '../General/Post';
 import MakePostForm from '../FeedPage/MakePostForm';
+import blankProfile from '../../assets/images/blankProfile.png';
 import '../../css/profile/Profile.css';
 
 const Profile = () => {
@@ -12,22 +13,22 @@ const Profile = () => {
     const { userName: username } = useParams();
     const [ error, setError ] = useState(null);
     const [ posts, setPosts ] = useState([]);
-    const [ fullName, setFullName ] = useState(null);
+    const [ user, setUser ] = useState({});
 
     const API = apiURL();
 
     const getUserPosts = async () => {
         try {
-            // Change currentUser.id to the username from params
-            // Change the request in the backend from id to username check
             let res = await axios.get(API + "/api/users/" + username + "/posts");
             setPosts(res.data.userPosts.map(post => {
                 return (
                         <Post post={post} key={post.id} />
                     )
                 }));
-            setFullName(res.data.userPosts[0].full_name);
+            let user = await axios.get(API + "/api/users/" + res.data.userPosts[0].poster_id);
+            setUser(user.data.user);
             setError(null);
+
         } catch(error) {
             if(error.response) {
                 setError(error.response.data.error);
@@ -40,39 +41,50 @@ const Profile = () => {
         getUserPosts();
     }, [])
 
-    const makePostSubmit = async (postBody, tags) => {
-        await axios.post(API + "/api/posts", {
-            poster_id: currentUser.id, 
-            body: postBody,
-            tags,
-            is_retweet: false, 
-            created_at: new Date().toString()
-        })
+    // const makePostSubmit = async (postBody, tags) => {
+    //     await axios.post(API + "/api/posts", {
+    //         poster_id: currentUser.id, 
+    //         body: postBody,
+    //         tags,
+    //         is_retweet: false, 
+    //         created_at: new Date().toString()
+    //     })
        
-        getUserPosts();
-    }
-    const displayMakePostForm = () => {
-        if(posts[0]) {
-            if(currentUser.id === posts[0].props.post.poster_id) {
-                return (
-                    <MakePostForm makePostSubmit={makePostSubmit}/>
-                )
-            } else {
-                return (
-                    null
-                )
-            }
-        } else {
-            return (
-                null
-            )
-        }
-    }
+    //     getUserPosts();
+    // }
 
+    // const displayMakePostForm = () => {
+    //     if(posts[0]) {
+    //         if(currentUser.id === posts[0].props.post.poster_id) {
+    //             return (
+    //                 <MakePostForm makePostSubmit={makePostSubmit}/>
+    //             )
+    //         } else {
+    //             return (
+    //                 null
+    //             )
+    //         }
+    //     } else {
+    //         return (
+    //             null
+    //         )
+    //     }
+    // }
+
+    const profilePic = user.profile_pic ? user.profile_pic : blankProfile;
+    
     return (
         <div className="profileContainer appCenter">
-            <header>{fullName ? fullName : "Profile"}</header>
-            {displayMakePostForm()}
+            <header>{user ? user.full_name : "Profile"}</header>
+            {/* {displayMakePostForm()} */}
+            <div className="profileUserInfo">
+                <img src={profilePic} alt={user.full_name} />
+                <div>
+                    <p className="profileFullName">{user.full_name}</p>
+                    <p className="profileUsername">@{user.username}</p>
+                </div>
+            </div>
+            <section>Posts</section>
             <div className="profilePostContainer">
                 {error ? <div className="error">{error}</div> : null}
                 {posts}
