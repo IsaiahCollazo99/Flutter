846f855492065b45e5dbf5ebf41b30f97cdcb310
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-27 06:42:36 -0400

    style DisplayPost Component

--- frontend/src/components/General/DisplayPost.js
+++ frontend/src/components/General/DisplayPost.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { apiURL } from '../../util/apiURL';
 import '../../css/general/DisplayPost.css';
+import { AuthContext } from '../../providers/AuthContext';
 
 const DisplayPost = () => {
     const { postId } = useParams();
     const [post, setPost] = useState({});
     const API = apiURL();
+    const history = useHistory();
+    const { currentUser } = useContext(AuthContext);
 
     const fetchPost = async () => {
         let res = await axios.get(API + "/api/posts/" + postId);
@@ -17,9 +20,28 @@ const DisplayPost = () => {
     useEffect(() => {
         fetchPost();
     })
+
+    const deletePost = async () => {
+        await axios.delete(API + "/api/posts/" + post.id);
+        history.push("/");
+    }
+
+    const displayDelete = () => {
+        if(currentUser) {
+            if(currentUser.id === post.poster_id) {
+                return (
+                    <p onClick={deletePost} className="deleteDisplay">Delete</p>
+                )
+            } else {
+                return (
+                    null
+                )
+            }
+        }
+    }
     
     return (
-        <div className="displayPost">
+        <div className="displayPost appCenter">
              <div className="displayPostContainer">
                 {/* <div className="displayPostLeft">
                     Profile Picture
@@ -30,11 +52,21 @@ const DisplayPost = () => {
                 </div> */}
 
                 <div className="displayPostInfo">
-                    <p className="displayPostName">{post.full_name}</p>
-                    <p className="displayPostUsername">@{post.username}</p>
+                    <div className="displayPostInfoLeft">
+                        <Link to={"/" + post.username} 
+                        className="displayPostName">
+                            {post.full_name}
+                        </Link>
+                        <p className="displayPostUsername">@{post.username}</p>
+                    </div>
+
+                    <div className="displayPostInfoRight">
+                        {displayDelete()}
+                    </div>
+
                 </div>
 
-                <div className="displayPost">
+                <div className="displayPostPost">
                     <div className="displayPostBody">
                         {post.body}
                     </div>
