0309c5db16ddda700ffae40b2bb6bfe84adc6bd7
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-05-27 10:02:32 -0400

    add profile picture functionality to UserCard

--- frontend/src/components/General/UserCard.js
+++ frontend/src/components/General/UserCard.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import blankProfile from '../../assets/images/blankProfile.png';
 import '../../css/general/UserCard.css';
 
 const UserCard = ({ user }) => {
+    let profilePic = user.profile_pic ? user.profile_pic : blankProfile;
     return (
         <div className="userCardContainer">
-            <Link to={"/" + user.username} className="userCardName">{user.full_name}</Link>
-            <p className="userCardUsername">@{user.username}</p>
+            <div className="userCardLeft">
+                <img src={profilePic} alt={user.name} />
+            </div>
+
+            <div className="userCardRight">
+                <Link to={"/" + user.username} className="userCardName">{user.full_name}</Link>
+                <p className="userCardUsername">@{user.username}</p>
+            </div>
+
         </div>
     )
 }
